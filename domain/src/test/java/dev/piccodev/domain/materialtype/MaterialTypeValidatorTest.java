e193ea319f666c0dcf6ad8aa5c0adb7831d19725
Rafiere <rafapgbr@gmail.com>
2023-12-20 20:12:45 -0300

    test: add name lesser than 3 and greater than 255 characters test

--- domain/src/test/java/dev/piccodev/domain/materialtype/MaterialTypeValidatorTest.java
+++ domain/src/test/java/dev/piccodev/domain/materialtype/MaterialTypeValidatorTest.java
@@ -68,4 +68,73 @@ public class MaterialTypeValidatorTest {
         Assertions.assertNotNull(createdMaterialType.getUpdatedAt());
         Assertions.assertNull(createdMaterialType.getDeletedAt());
     }
+
+    @Test
+    public void givenAnInvalidLesserThan3CharactersName_whenCallNewMaterialType_thenReceiveAnError(){
+
+        final String expectedName = "Do ";
+        final var expectedDescription =
+                """
+                A documentary film is a non-fictional motion-picture intended to document
+                reality, primarily for the purposes of instruction, education, or
+                maintaining a historical record.
+                """;
+        final var expectedIsDeleted = false;
+
+        final var createdMaterialType = MaterialType.newMaterialType(expectedName, expectedDescription, expectedIsDeleted);
+
+        final var thrownException = Assertions.assertThrows(DomainException.class, () -> createdMaterialType.validate(new ThrowsValidationHandler()));
+        final var expectedExceptionMessage = "'name' must be between 3 and 255 characters";
+        Assertions.assertEquals(thrownException.getErrors().get(0).message(), expectedExceptionMessage);
+
+        final var expectedErrorCount = 1;
+        Assertions.assertEquals(thrownException.getErrors().size(), expectedErrorCount);
+
+        Assertions.assertNotNull(createdMaterialType);
+        Assertions.assertEquals(createdMaterialType.getName(), expectedName);
+        Assertions.assertEquals(createdMaterialType.getDescription(), expectedDescription);
+        Assertions.assertFalse(createdMaterialType.isDeleted());
+
+        Assertions.assertNotNull(createdMaterialType.getCreatedAt());
+        Assertions.assertNotNull(createdMaterialType.getUpdatedAt());
+        Assertions.assertNull(createdMaterialType.getDeletedAt());
+    }
+
+    @Test
+    public void givenAnInvalidGreaterThan255CharactersName_whenCallNewMaterialType_thenReceiveAnError(){
+
+        final String expectedName =
+                """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                ut cursus lectus. Ut sed sollicitudin sem, a dictum lacus. Suspendisse
+                potenti. Proin sit amet dapibus sapien. Ut sit amet tortor efficitur, commodo
+                nisl, elementum eros. Nulla pellentesque turpis a urna volutpat, ac mollis sem
+                lacinia. Praesent auctor fermentum gravida.
+                """;
+        final var expectedDescription =
+                """
+                A documentary film is a non-fictional motion-picture intended to document
+                reality, primarily for the purposes of instruction, education, or
+                maintaining a historical record.
+                """;
+        final var expectedIsDeleted = false;
+
+        final var createdMaterialType = MaterialType.newMaterialType(expectedName, expectedDescription, expectedIsDeleted);
+
+        final var thrownException = Assertions.assertThrows(DomainException.class, () -> createdMaterialType.validate(new ThrowsValidationHandler()));
+        final var expectedExceptionMessage = "'name' must be between 3 and 255 characters";
+        Assertions.assertEquals(thrownException.getErrors().get(0).message(), expectedExceptionMessage);
+
+        final var expectedErrorCount = 1;
+        Assertions.assertEquals(thrownException.getErrors().size(), expectedErrorCount);
+
+        Assertions.assertNotNull(createdMaterialType);
+        Assertions.assertEquals(createdMaterialType.getName(), expectedName);
+        Assertions.assertEquals(createdMaterialType.getDescription(), expectedDescription);
+        Assertions.assertFalse(createdMaterialType.isDeleted());
+
+        Assertions.assertNotNull(createdMaterialType.getCreatedAt());
+        Assertions.assertNotNull(createdMaterialType.getUpdatedAt());
+        Assertions.assertNull(createdMaterialType.getDeletedAt());
+    }
 }
